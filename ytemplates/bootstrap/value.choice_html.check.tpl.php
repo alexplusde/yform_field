693825819cefd9d4d19875b1924a87bc74ece4ec
Alexander Walther <aw@alexplus.de>
2022-04-13 10:30:27 +0200

    choice with html ouput
    https://github.com/yakamara/redaxo_yform/pull/896

---
+++ ytemplates/bootstrap/value.choice_html.check.tpl.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * @var rex_yform_value_abstract $this
+ * @psalm-scope-this rex_yform_value_abstract
+ * @var rex_yform_choice_list $choiceList
+ * @var rex_yform_choice_list_view $choiceListView
+ */
+
+$notices = [];
+if ($this->getElement('notice')) {
+    $notices[] = rex_i18n::translate($this->getElement('notice'), false);
+}
+if (isset($this->params['warning_messages'][$this->getId()]) && !$this->params['hide_field_warning_messages']) {
+    $notices[] = '<span class="text-warning">'.rex_i18n::translate($this->params['warning_messages'][$this->getId()], false).'</span>';
+}
+
+if (!isset($groupAttributes)) {
+    $groupAttributes = [];
+}
+
+$groupClass = 'form-check-group';
+if (isset($groupAttributes['class']) && is_array($groupAttributes['class'])) {
+    $groupAttributes['class'][] = $groupClass;
+} elseif (isset($groupAttributes['class'])) {
+    $groupAttributes['class'] .= ' '.$groupClass;
+} else {
+    $groupAttributes['class'] = $groupClass;
+}
+
+if (!isset($elementAttributes)) {
+    $elementAttributes = [];
+}
+$elementClass = trim(($choiceList->isMultiple() ? 'checkbox' : 'radio').' '.$this->getWarningClass());
+if (isset($elementAttributes['class']) && is_array($elementAttributes['class'])) {
+    $elementAttributes['class'][] = $elementClass;
+} elseif (isset($elementAttributes['class'])) {
+    $elementAttributes['class'] .= ' '.$elementClass;
+} else {
+    $elementAttributes['class'] = $elementClass;
+}
+
+?>
+
+<?php $choiceOutput = function (rex_yform_choice_view $view) use ($elementAttributes) {
+    ?>
+<div<?= rex_string::buildAttributes($elementAttributes) ?>>
+    <label>
+        <input value="<?= rex_escape($view->getValue()) ?>" <?= (in_array($view->getValue(), $this->getValue(), true) ? ' checked="checked"' : '') ?>
+        <?= $view->getAttributesAsString() ?>
+        />
+        <i class="form-helper"></i>
+        <?= $view->getLabel() ?>
+    </label>
+    </div>
+    <?php
+} ?>
+
+    <?php $choiceGroupOutput = static function (rex_yform_choice_group_view $view) use ($choiceOutput) {
+        ?>
+    <div class="form-check-group">
+        <label><?= $view->getLabel() ?></label>
+        <?php foreach ($view->getChoices() as $choiceView): ?>
+        <?php $choiceOutput($choiceView) ?>
+        <?php endforeach ?>
+    </div>
+    <?php
+    } ?>
+
+    <?php
+    if (!isset($groupAttributes['id'])) {
+        $groupAttributes['id'] = $this->getHTMLId();
+    }
+ ?>
+
+    <div<?= rex_string::buildAttributes($groupAttributes) ?>>
+        <?php if ($this->getLabel()): ?>
+        <label class="control-label"
+            for="<?= $this->getFieldId() ?>">
+            <?= $this->getLabelStyle($this->getLabel()) ?>
+        </label>
+        <?php endif ?>
+
+        <?php foreach ($choiceListView->getPreferredChoices() as $view): ?>
+        <?php $view instanceof rex_yform_choice_group_view ? $choiceGroupOutput($view) : $choiceOutput($view) ?>
+        <?php endforeach ?>
+
+        <?php foreach ($choiceListView->getChoices() as $view): ?>
+        <?php $view instanceof rex_yform_choice_group_view ? $choiceGroupOutput($view) : $choiceOutput($view) ?>
+        <?php endforeach ?>
+
+        <?php if ($notices): ?>
+        <p class="help-block small"><?= implode('<br />', $notices) ?>
+        </p>
+        <?php endif ?>
+        </div>
