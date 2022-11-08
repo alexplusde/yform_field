b0bc6ad4f66a6cb1e0ebe421ee37321b7727b6ed
Alexander Walther <github.com@alexplus.de>
2022-11-08 12:44:04 +0100

    Update session.php

--- lib/yform/action/session.php
+++ lib/yform/action/session.php
@@ -1,15 +1,20 @@
-class rex_yform_action_saveinsession extends rex_yform_action_abstract
+class rex_yform_action_tosession extends rex_yform_action_abstract
 {
-    public function executeAction() :void
-    {
-			foreach(
-				$this->params['value_pool']['sql'] as $key => $value) {
-					$_SESSION['yform']['session'][$key] = htmlspecialchars($value);
-			}
+	public function executeAction() :void
+	{
+		/* TODO: Session starten, falls noch nicht geschehen? */
+		foreach($this->params['value_pool']['sql'] as $key => $value) 
+		{
+			/* TODO: rex_set_session('meine_var', $meine_var) - Tabellen-Key berücksichtigen, um Action mehrfach einsetzen zu können. */
+			$_SESSION['yform']['session'][$key] = htmlspecialchars($value);
 		}
+	}
 
-    public function getDescription() :string
-    {
-        return "action|saveinsession <b>[Session Plugin]</b> --> Schreibt Feldwerte in $SESSION";
-    }
+	public function getDescription() :string
+	{
+		return "action|tosession <b>Schreibt alle Feldwerte in $SESSION";
+	}
+	/* TODO: Statische Methode zum Auslesen der $SESSION bereitstellen. */
 }
+
+	
