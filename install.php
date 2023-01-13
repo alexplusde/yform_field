2808c3c57f23838b56bc2f5f9ce2ae7a142e4249
christophboecker <christoph_boecker@t-online.de>
2023-01-13 09:56:33 +0100

    Bei Install/Update SCSS zu CSS kompilieren

---
+++ install.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * @var rex_addon $this
+ */
+
+/**
+ * Erstellt eine CSS-Datei basierend auf den Backend-Styles aus dem Addon be_style (falls aktiv).
+ * rex_scss_compiler ist verfügbar wenn be_style installiert ist.
+ */
+if (class_exists('rex_scss_compiler')) {
+    $compiler = new rex_scss_compiler();
+
+    if( rex::isDebugMode() || false === $this->getProperty('compress_assets',true)) {
+        // Klartext-Ausgabe falls man für Tests "lesbares" CSS erzeugen möchte
+        $compiler->setFormatter(\ScssPhp\ScssPhp\Formatter\Expanded::class);
+    }
+
+    $compiler->setRootDir(__DIR__ . '/scss');
+    $compiler->setScssFile([
+        rex_path::plugin('be_style', 'redaxo', 'scss/_variables.scss'),
+        rex_path::plugin('be_style', 'redaxo', 'scss/_variables-dark.scss'),
+        rex_path::addon('be_style', 'vendor/font-awesome/scss/_variables.scss'),
+        __DIR__ . '/scss/be.scss',
+    ]);
+
+    $compiler->setCssFile(__DIR__ . '/assets/be.min.css');
+    $compiler->compile();
+}
