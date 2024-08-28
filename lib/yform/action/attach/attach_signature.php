b4a1857d037c597734320916a4bc348fd45aa6f6
Alexander Walther <aw@alexplus.de>
2024-08-28 16:41:26 +0200

    Update attach_signature.php

--- lib/yform/action/attach/attach_signature.php
+++ lib/yform/action/attach/attach_signature.php
@@ -2,32 +2,31 @@
 
 class rex_yform_action_attach_signature extends rex_yform_action_abstract
 {
+
     public function executeAction(): void
     {
-        $fieldname = $this->getElement(2) ?? '';
-        $filenname = $this->getElement(3) ?? 'signature.png';
-
-        if ('' == $fieldname) {
+        $fieldName = $this->getElement(2) ?: '';
+        if ($fieldName == '') {
             return;
         }
+        $fileName = $this->getElement(3) ?: $fieldName . '.png';
 
-        $signatureValue = $this->params['value_pool']['email'][$fieldname];
+        $signatureValue = $this->params['value_pool']['email'][$fieldName];
         $base64String = str_replace('data:image/png;base64,', '', $signatureValue);
         $imageData = base64_decode(str_replace(' ', '+', $base64String));
 
         try {
-            rex_file::put(rex_path::cache($filenname), $imageData);
-            if (file_exists(rex_path::cache($filenname))) {
-                $this->params['value_pool']['email_attachments']['signature_attach'] = [0 => $filenname, 1 => rex_path::cache($filenname)];
+            rex_file::put(rex_path::cache($fileName), $imageData);
+            if (file_exists(rex_path::cache($fileName))) {
+                $this->params['value_pool']['email_attachments'][] = [ 0 => $fileName, 1 => rex_path::cache($fileName)];
             }
         } catch (Exception $e) {
             echo 'Caught exception: ',  $e->getMessage(), "\n";
         }
-        rex_file::delete(rex_path::cache($filenname));
     }
 
     public function getDescription(): string
     {
-        return 'action|attach_signature|fieldname|[filename]';
+        return 'action|signature_attach|fieldname|[filename]';
     }
 }
