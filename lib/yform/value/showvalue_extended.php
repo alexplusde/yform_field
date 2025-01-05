640de41bb93f0c3e51ff1f02222acf291766d140
Alexander Walther <mail@alexplus.de>
2025-01-06 00:31:56 +0100

    new field: showvalue_extended

---
+++ lib/yform/value/showvalue_extended.php
@@ -0,0 +1,26 @@
+<?php
+
+class rex_yform_value_showvalue_extended extends rex_yform_value_showvalue
+{
+    public function getDescription(): string
+    {
+        return 'showvalue_extended|name|label|defaultwert|notice';
+    }
+
+    public function getDefinitions(): array
+    {
+        return [
+            'type' => 'value',
+            'name' => 'showvalue_extended',
+            'values' => [
+                'name' => ['type' => 'name',    'label' => rex_i18n::msg('yform_values_defaults_name')],
+                'label' => ['type' => 'text',    'label' => rex_i18n::msg('yform_values_defaults_label')],
+                'default' => ['type' => 'text',    'label' => rex_i18n::msg('yform_values_text_default')],
+                'notice' => ['type' => 'text',    'label' => rex_i18n::msg('yform_values_defaults_notice')],
+            ],
+            'description' => rex_i18n::msg('yform_values_showvalue_extended_description'),
+            'db_type' => ['text', 'varchar(191)', 'mediumtext', 'longtext'],
+        ];
+    }
+
+}
