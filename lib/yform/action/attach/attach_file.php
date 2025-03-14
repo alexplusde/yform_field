a5ccf9d080ee08d9beb1e5bd9602323ee375126d
Alexander Walther <mail@alexplus.de>
2025-03-14 23:13:20 +0100

    Create attach_file.php

---
+++ lib/yform/action/attach/attach_file.php
@@ -0,0 +1,30 @@
+<?php
+
+class rex_yform_action_attach extends rex_yform_action_abstract
+{
+    public function executeAction(): void
+    {
+        $file = array_filter(explode(',', $this->getElement(2))); // specific fields
+        $override = (bool) $this->getElement(3) ?? false; // will replace attached files
+        $email_attachments = &$this->params['value_pool']['email_attachments']; // attached files
+
+        $attachment = ['filename' => $file[0], 'path' => rex_path::addonData($file[1])];
+
+        // Prevent path traversal
+        $realPath = realpath($attachment['path']);
+        $allowedPath = realpath(rex_path::addonData(''));
+
+        if ($realPath && strpos($realPath, $allowedPath) === 0 && file_exists($realPath)) {
+            $attachment['path'] = $realPath;
+            if ($override) {
+                $email_attachments = [];
+            }
+            $email_attachments[] = $attachment;
+        }
+    }
+
+    public function getDescription(): string
+    {
+        return 'action|attach_file|file(attachment_filename.ext,path/to/file.ext)|opt:replace(0=default/1)';
+    }
+}
