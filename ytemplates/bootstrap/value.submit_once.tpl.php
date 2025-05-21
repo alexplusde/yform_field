26dc69aab0b9521522c30a692b067d03557f388a
Alexander Walther <aw@alexplus.de>
2025-05-21 23:11:35 +0200

    nonce zu submit_once hinzufügen
    Signed-off-by: Alexander Walther <aw@alexplus.de>

--- ytemplates/bootstrap/value.submit_once.tpl.php
+++ ytemplates/bootstrap/value.submit_once.tpl.php
@@ -20,11 +20,19 @@ if ('' != $this->getWarningClass()) {
 $id = $this->getFieldId() . '-' . rex_string::normalize($label);
 $label_translated = rex_i18n::translate($label, true);
 ?>
-<button onclick="
+<button
+    class="<?= implode(' ', $classes) ?>"
+    type="submit"
+    name="<?= $this->getFieldName() ?>"
+    id="<?= $id ?>"
+>
+<?= $label_translated ?>
+</button>
+<script nonce="<?= rex_response::getNonce() ?>">
+document.getElementById('<?= $id ?>').addEventListener('click', function(e) {
     history.pushState({ page: 1 }, '', '#');
     this.disabled = true;
     this.value = '<?= $loading ?>';
-    HTMLFormElement.prototype.submit.call(this.form);"
-    class="<?= implode(' ', $classes) ?>" type="submit" name="<?= $this->getFieldName() ?>" id="<?= $id ?>">
-<?= $label_translated ?>
-</button>
+    HTMLFormElement.prototype.submit.call(this.form);
+});
+</script>
